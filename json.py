a64861d6915be97dcb47807c48b91515bd5f5c0a
Ashutosh Jasrotia <89795159+Ashu1864@users.noreply.github.com>
2022-01-12 12:32:16 +0530

    Add files via upload

--- json.py
+++ json.py
@@ -1,9 +1,13 @@
-import requests, json
+import simplejson as json
 from elasticsearch import Elasticsearch
-res = requests.get('http://localhost:9200')
-print (res.content)
-es = Elasticsearch(host = "localhost", port = 9200)
+
+es = Elasticsearch(host="localhost", port=9200)
 with open('csvjson.json') as data_file:
     data = json.load(data_file)
-    for result in data:
-        print (result[result][0])
+    file = len(data)
+    for results in data:
+        for x in range(0, file):
+            ind = results['result'][x]['cve/CVE_data_meta/ID']
+            index = results['result'][x]
+            es.indices.create(index=ind)
+            es.index(index=ind, doc_type='CVE', id=ind, body=json.loads(index.content))
