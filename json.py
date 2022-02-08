c7ad636171204312af771957548f31cc0ba4f085
Ashutosh Jasrotia <89795159+Ashu1864@users.noreply.github.com>
2022-02-08 17:59:16 +0530

    Update json.py

--- json.py
+++ json.py
@@ -1,13 +1,15 @@
 import simplejson as json
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch(host="localhost", port=9200)
-with open('csvjson.json') as data_file:
-    data = json.load(data_file)
-    file = len(data)
-    for results in data:
-        for x in range(0, file):
-            ind = results['result'][x]['cve/CVE_data_meta/ID']
-            index = results['result'][x]
-            es.indices.create(index=ind)
-            es.index(index=ind, doc_type='CVE', id=ind, body=json.loads(index.content))
+res = requests.get('http://localhost:9200')
+print (res.content)
+es = Elasticsearch([{'host': 'localhost', 'port': '9200'}])
+i = 1
+for filename in os.listdir(directory):
+    if filename.endswith(".json"):
+        f = open(filename)
+        docket_content = f.read()
+        # Send the data into es
+        es.index(index='cveindex', ignore=400, doc_type='docket', 
+        id=i, body=json.loads(docket_content))
+        i = i + 1
