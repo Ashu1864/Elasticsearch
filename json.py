8148aefb57d9fbd62a9dc4e0b4dddae34f2170cb
Ashutosh Jasrotia <89795159+Ashu1864@users.noreply.github.com>
2022-02-09 12:44:23 +0530

    Update json.py

--- json.py
+++ json.py
@@ -1,18 +1,34 @@
 import requests, json, os
 from elasticsearch import Elasticsearch
 
-directory = '/media/ashu/New Volume1/Elasticsearch-main/'
+directory = '/home/ashutosh/src/elastic/'
 res = requests.get('http://192.168.1.17:9200')
 print (res.content)
 es = Elasticsearch([{'host': '192.168.1.17', 'port': '9200'}])
-i = 1
-for filename in os.listdir(directory):
-    if filename.endswith(".json"):
-        f = open(filename)
-        docket_content = f.read()
-        # Send the data into es
-        es.index(index='cveindex', ignore=400, doc_type='docket', 
-        cveid=i, body=json.loads(docket_content))
-        i = i + 1
-data = es.get(index="cveindex", doc_type='docket', id=4)
-print(data)
+check=es.indices.exists(index="cveindex")
+print (check)
+if check is True :
+    i = 1
+    for filename in os.listdir(directory):
+        if filename.endswith(".json"):
+            f = open(filename)
+            docket_content = f.read()
+            # Send the data into es
+            es.index(index='cveindex', ignore=400, doc_type='docket', 
+            id=i, body=json.loads(docket_content))
+            i = i + 1
+    data = es.get(index="cveindex", doc_type='docket', id=4)
+    print(data)
+else :
+    es.indices.create(index="cveindex" , ignore=400)
+    i = 1
+    for filename in os.listdir(directory):
+        if filename.endswith(".json"):
+            f = open(filename)
+            docket_content = f.read()
+            # Send the data into es
+            es.index(index='cveindex', ignore=400, doc_type='docket',
+            id=i, body=json.loads(docket_content))
+            i = i + 1
+    data = es.get(index="cveindex", doc_type='docket', id=4)
+    print(data)
